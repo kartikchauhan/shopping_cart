370935784ae54da97d4c0eeef7b4b244f00f7885
kartik chauhan <chauhan.kartik25@gmail.com>
2017-04-22 02:45:06 +0530

    routes grouping done - added global state for checking authentication in app.js

--- app.js
+++ app.js
@@ -34,8 +34,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+	res.locals.loggedIn = req.isAuthenticated();
+	next();
+});
+
+app.use('/user', users);
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var assert = require('assert');
-var csrf = require('csurf');
 var passport = require('passport');
 
 var Product = require('../models/product');
-
-var csrfProtection = csrf();
-router.use(csrfProtection);
 
 router.get('/', function(req, res, next) {
 	Product.find(function(error, docs) {
@@ -21,32 +17,4 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-// Signup routes
-router.get('/user/signup', function(req, res, next) {
-	var messages = req.flash('error');
-	res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasError: messages.length > 0});
-});
-
-router.post('/user/signup', passport.authenticate('local.signup', {
-	successRedirect: '/user/profile',
-	failureRedirect: '/user/signup',
-	failureFlash : true
-}));
-
-// Signin routes
-router.get('/user/signin', function(req, res, next) {
-	var messages = req.flash('error');
-	res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasError: messages.length > 0});
-});
-
-router.post('/user/signin', passport.authenticate('local.signin', {
-	successRedirect: '/user/profile',
-	failureRedirect: '/user/signin',
-	failureFlash: true
-}));
-
-// User profile routes
-router.get('/user/profile', function(req, res, next) {
-	res.render('user/profile');
-});
 module.exports = router;
