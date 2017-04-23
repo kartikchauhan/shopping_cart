7f5be2b5b28efdbf5439885f7f24f6c761013a58
kartik chauhan <chauhan.kartik25@gmail.com>
2017-04-24 02:29:29 +0530

    stripe test payment gateway completely added - flash messages added for UX

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ var passport = require('passport');
 
 var Product = require('../models/product');
 var Cart = require('../models/cart');
+
 router.get('/', function(req, res, next) {
+
+	var successMsg = req.flash('success')[0];
 	Product.find(function(error, docs) {
 		var chunkSize = 3;
 		var products = [];
@@ -13,7 +16,7 @@ router.get('/', function(req, res, next) {
 		{
 			products.push(docs.slice(i, i+chunkSize));
 		}
-  		res.render('shop/index', { title: 'Shopping Cart', products: products });
+  		res.render('shop/index', { title: 'Shopping Cart', products: products, successMsg: successMsg, noMsg: !successMsg });
 	});	
 });
 
@@ -48,7 +51,35 @@ router.get('/checkout', function(req, res, next) {
 		return res.redirect('/shopping-cart');
 	}
 	var cart = new Cart(req.session.cart);
-	res.render('shop/checkout', {totalPrice: cart.totalPrice});
+	var errMsg = req.flash('error')[0];
+	res.render('shop/checkout', {totalPrice: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
+});
+
+router.post('/checkout', function(req, res, next) {
+	if(!req.session.cart)
+	{
+		return res.redirect('/shopping-cart');
+	}
+	var cart = new Cart(req.session.cart);
+
+	var stripe = require("stripe")(
+	  	"sk_test_3fMEErbX3cilua709xH57qI6"
+	);
+	stripe.charges.create({
+	  	amount: cart.totalPrice * 100,
+	  	currency: "usd",
+	  	source: req.body.stripeToken, // obtained with Stripe.js
+	  	description: "Test customer charging"
+	}, function(err, charge) {
+	  	if(err)
+	  	{
+	  		req.flash('error', err.message);
+	  		return res.redirect('/checkout');
+	  	}
+	  	req.flash('success', 'Payment Successful');
+	  	req.cart = null;
+	  	res.redirect('/');
+	});
 });
 
 module.exports = router;
